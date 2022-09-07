578ac6cae78955cb76becec1960f74c0f688e016
MauricioLZ <mauriciolopeszugno_1@hotmail.com>
2022-09-07 12:50:10 +0100

    Contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+
 function Contact()
 {
+    const [message, setMessage] = useState("");
+    const [sentMsg, setSentMsg] = useState(false);
+
+    function writeMessage(e) {
+        const target = e.target;
+        setMessage(target.value);
+    }
+
+    function onSubmit(e) {
+        setSentMsg(true);
+        e.preventDefault();
+    }
+
     return (
         <div className="contact">
-
+            <h2>Contact</h2>
+            <p className="contactDetails">
+                Mauricio Lopes Zugno
+                <br/>Ireland, Cork city
+                <br/>(+353) 085 172 9759
+                <br/>mauriciolopz@gmail.com
+            </p>
+            <p className="contactInstructions">Get in touch by sending me a message through here:</p>
+            <form onSubmit={onSubmit}>
+                <textarea rows="6" name="message" onChange={writeMessage} value={message}></textarea>
+                <div className="contactSubmitContainer">
+                    { sentMsg && <p className="contactFeedback">Thank you, your message has been sent!</p> }
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
     );
 }
