b60a963796ddb0e5de5b68a80fb430ffe9b59889
MauricioLZ <mauriciolopeszugno_1@hotmail.com>
2022-09-07 15:49:48 +0100

    Functional email sending

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,18 +1,44 @@
 import { useState } from "react";
+import $ from 'jquery';
+import emailjs from '@emailjs/browser';
 
 function Contact()
 {
     const [message, setMessage] = useState("");
-    const [sentMsg, setSentMsg] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [submitMsg, setSubmitMsg] = useState("");
 
     function writeMessage(e) {
         const target = e.target;
         setMessage(target.value);
     }
 
+    function writeName(e) {
+        const target = e.target;
+        setName(target.value);
+    }
+
+    function writeEmail(e) {
+        const target = e.target;
+        setEmail(target.value);
+    }
+
     function onSubmit(e) {
-        setSentMsg(true);
         e.preventDefault();
+        const feedbackMsg = $(".contactFeedback")[0];
+        const target = e.target;
+        emailjs.sendForm(process.env.REACT_APP_EMAIL_SERVICE_ID, process.env.REACT_APP_EMAIL_TEMPLATE_ID, target, process.env.REACT_APP_EMAIL_PUBLIC_KEY)
+            .then((result) =>
+            {
+                feedbackMsg.className = "contactFeedback colorOk";
+                setSubmitMsg("Thank you, your message has been sent!");
+            },
+            (error) =>
+            {
+                feedbackMsg.className = "contactFeedback colorError";
+                setSubmitMsg("An error occurred, please try again.");
+            });
     }
 
     return (
@@ -24,11 +50,13 @@ function Contact()
                 <br/>(+353) 085 172 9759
                 <br/>mauriciolopz@gmail.com
             </p>
-            <p className="contactInstructions">Get in touch by sending me a message through here:</p>
+            <p className="contactInstructions">Get in touch by sending me a message through the box below. If you want to be contacted back please write your name and email as well.</p>
             <form onSubmit={onSubmit}>
-                <textarea rows="6" name="message" onChange={writeMessage} value={message}></textarea>
+                <input type="text" name="name" placeholder="Name" onChange={writeName} value={name}></input>
+                <input type="email" name="email" placeholder="Email" onChange={writeEmail} value={email}></input>
+                <textarea rows="6" name="message" placeholder="Message..." onChange={writeMessage} value={message}></textarea>
                 <div className="contactSubmitContainer">
-                    { sentMsg && <p className="contactFeedback">Thank you, your message has been sent!</p> }
+                    <p className="contactFeedback">{submitMsg}</p>
                     <button type="submit">Submit</button>
                 </div>
             </form>
